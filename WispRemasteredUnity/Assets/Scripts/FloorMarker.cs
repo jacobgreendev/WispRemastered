a0e90f7808d11727d4ea3a5ca5582c7d07629c50
Jacob Green <56412581+jacobgreendev@users.noreply.github.com>
2021-08-01 23:24:57 +0100

    Added fade in/out via corouting when firing/landing using OnFire and OnLand events

--- WispRemasteredUnity/Assets/Scripts/FloorMarker.cs
+++ WispRemasteredUnity/Assets/Scripts/FloorMarker.cs
@@ -8,11 +8,20 @@ public class FloorMarker : MonoBehaviour
     private DecalProjector projector;
     [SerializeField] private float minSize, maxSize;
 
+    [Header("Fading In/Out")]
+    [SerializeField] private float fadeTime;
+    private Coroutine currentFadeRoutine;
+    private Material decalMaterial;
+
     // Start is called before the first frame update
     void Start()
     {
         projector = GetComponent<DecalProjector>();
         PlayerController.Instance.PlayerPositionUpdated += UpdateFloorMarkerSize;
+        PlayerController.Instance.OnLand += FadeOutFloorMarker;
+        PlayerController.Instance.OnFire += FadeInFloorMarker;
+        UpdateFloorMarkerSize(PlayerController.Instance.transform.position);
+        decalMaterial = projector.material;
     }
 
     // Update is called once per frame
@@ -27,7 +36,44 @@ public class FloorMarker : MonoBehaviour
             var newSize = Mathf.Lerp(minSize, maxSize, distance / currentSize.z);
             currentSize.x = newSize;
             currentSize.y = newSize;
-            projector.size = currentSize;
+        }
+        else
+        {
+            currentSize.x = minSize;
+            currentSize.y = minSize;
+        }
+
+        projector.size = currentSize;
+    }
+
+    private void FadeOutFloorMarker(Transform landedOn)
+    {
+        if (currentFadeRoutine != null)
+        {
+            StopCoroutine(currentFadeRoutine);
+        }
+        currentFadeRoutine = StartCoroutine(FadeFloorMarker(decalMaterial.GetFloat("_Alpha"), 0, fadeTime));
+    }
+
+    private void FadeInFloorMarker()
+    {
+        if(currentFadeRoutine != null)
+        {
+            StopCoroutine(currentFadeRoutine);
+        }
+        currentFadeRoutine = StartCoroutine(FadeFloorMarker(decalMaterial.GetFloat("_Alpha"), 1, fadeTime));
+    }
+
+    private IEnumerator FadeFloorMarker(float initialAlpha, float targetAlpha, float fadeTime)
+    {
+        float time = 0;
+        float newAlpha;
+        while (time < fadeTime)
+        {
+            newAlpha = Mathf.Lerp(initialAlpha, targetAlpha, time / fadeTime);
+            decalMaterial.SetFloat("_Alpha", newAlpha);
+            time += Time.deltaTime;
+            yield return null;
         }
     }
 }
