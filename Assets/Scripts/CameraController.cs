88121dfe178c83ed7087b95931cb4b9053e5d484
Jacob Green <56412581+jacobgreendev@users.noreply.github.com>
2021-08-01 14:37:25 +0100

    Smoothly follows player by arbitrary offset
    Also triggers CameraPositionUpdated if it moves

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    public static CameraController Instance;
+
+    public event CameraPositionUpdatedEventHandler CameraPositionUpdated;
+
+    [SerializeField] private float followLerpSpeed;
+    private Transform following;
+    private Vector3 offset;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Instance = this;
+        following = PlayerController.Instance.transform;
+        offset = transform.position - following.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        var currentPosition = transform.position;
+        var targetPosition = following.position + offset;
+
+        if (Vector3.Distance(currentPosition, targetPosition) > 0)
+        {
+            transform.position = Vector3.Lerp(currentPosition, targetPosition, followLerpSpeed * Time.deltaTime);
+            CameraPositionUpdated(transform.position);
+        }
+    }
+
+    public delegate void CameraPositionUpdatedEventHandler(Vector3 position);
+}
