353e8446a4d4755f3e4abf8ef7159a5dc3fb778b
Jacob Green <56412581+jacobgreendev@users.noreply.github.com>
2021-08-01 13:11:48 +0100

    Added UI manager
    Added UI manager, currently only listens to DragPositionUpdated event from PlayerController to display the drag indicator on the UI

---
+++ Assets/Scripts/UIManager.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UIManager : MonoBehaviour
+{
+    [SerializeField] private readonly LineRenderer dragLineRenderer;
+    private Camera mainCamera;
+    private Vector2 screenCentre = new(Screen.width / 2, Screen.height / 2);
+
+    private void Awake()
+    {
+        
+    }
+
+    // Start is called before the first frame update
+    private void Start()
+    {
+        PlayerController.Instance.DragPositionUpdated += OnDragPositionUpdate;
+        mainCamera = Camera.main;
+        dragLineRenderer.SetPosition(0, screenCentre); //Position is relative to line renderer position, which in this case is the bottom left
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+
+    }
+
+    void OnDragPositionUpdate(Vector2 position)
+    {
+        Debug.Log($"Input drag position: {position}");
+        dragLineRenderer.SetPosition(1, position);
+    }
+}
