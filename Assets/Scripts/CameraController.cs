2eac2ba5e0e1fd9a1449a405b81f8db13e30c111
Jacob Green <56412581+jacobgreendev@users.noreply.github.com>
2021-08-01 19:47:27 +0100

    FOV changes with speed using OnPlayerVelocityUpdate

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -8,20 +8,30 @@ public class CameraController : MonoBehaviour
 
     public event CameraPositionUpdatedEventHandler CameraPositionUpdated;
 
-    [SerializeField] private float followLerpSpeed;
     private Transform following;
     private Vector3 offset;
+    private new Camera camera;
+
+    [Header("Lerp Speeds")]
+    [SerializeField] private float followLerpSpeed;
+
+    [Header("FOV")]
+    [SerializeField] private float defaultFov;
+    [SerializeField] private float fovPerUnitSpeed, fovLerpSpeed;
+
 
     // Start is called before the first frame update
     void Awake()
     {
         Instance = this;
+        camera = GetComponent<Camera>();
     }
 
     private void Start()
     {
         following = PlayerController.Instance.transform;
         offset = transform.position - following.position;
+        PlayerController.Instance.VelocityUpdated += OnPlayerVelocityUpdate;
     }
 
     // Update is called once per frame
@@ -35,6 +45,10 @@ public class CameraController : MonoBehaviour
             transform.position = Vector3.Lerp(currentPosition, targetPosition, followLerpSpeed * Time.deltaTime);
             CameraPositionUpdated(transform.position);
         }
+    }
+    private void OnPlayerVelocityUpdate(Vector3 velocity)
+    {
+        camera.fieldOfView = Mathf.Lerp(camera.fieldOfView, defaultFov + (fovPerUnitSpeed * velocity.z), fovLerpSpeed * Time.deltaTime);
     }
 
     public delegate void CameraPositionUpdatedEventHandler(Vector3 position);
