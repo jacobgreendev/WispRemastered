9e46caf77b000884c4a06c0a32313533c011e2fe
zemiguel99 <zemiguel99@hotmail.com>
2021-08-03 20:26:36 +0100

    Added Slow-mo effect at the end of the firework interactable

--- WispRemasteredUnity/Assets/Scripts/Interactables/Firework.cs
+++ WispRemasteredUnity/Assets/Scripts/Interactables/Firework.cs
@@ -1,5 +1,4 @@
 using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class Firework : Interactable
@@ -7,12 +6,19 @@ public class Firework : Interactable
     [SerializeField] private float landLerpTime, distance, fuseTime, travelTime, explosionForce;
     [SerializeField] private Transform parentTransform, fuseStart, fuseEnd;
 
+    [Header("Slow-motion")]
+    [SerializeField] private float slowMoTime = 0.5f, targetTimeScale = 0.3f;
+
+    private PlayerController player;
+
     public override void DoInteraction(PlayerController player)
     {
-        StartCoroutine(RideFirework(player));
+        this.player = player;
+
+        StartCoroutine(RideFirework());
     }
 
-    private IEnumerator RideFirework(PlayerController player)
+    private IEnumerator RideFirework()
     {
         player.ResetJourney();
         CameraController.Instance.DisableBirdseye();
@@ -30,7 +36,6 @@ public class Firework : Interactable
 
         var endPosition = parentTransform.position + parentTransform.up * distance;
 
-
         player.transform.parent = parentTransform;
         yield return StartCoroutine(player.SmoothLerpToPosition(parentTransform, travelTime, parentTransform.position, endPosition));
         player.transform.parent = null;
@@ -38,9 +43,34 @@ public class Firework : Interactable
         player.Rigidbody.isKinematic = false;
         player.Land(transform, inPlace: true);
         player.Rigidbody.velocity = parentTransform.up * explosionForce;
-        player.IsInteracting = false;
-        Destroy(parentTransform.gameObject);
+        player.IsInteracting = false;       
+
+        //Slow Down time
+        yield return StartCoroutine(SmoothSlowDownTime());
+        PlayerController.Instance.OnFire += OnPlayerFired;
     }
 
 
+    void OnPlayerFired()
+    {
+        PlayerController.Instance.OnFire -= OnPlayerFired;
+
+        Time.timeScale = 1;
+        Destroy(parentTransform.gameObject);
+    }
+
+    IEnumerator SmoothSlowDownTime()
+    {
+        float time = 0f;
+        float initialTimeScale = Time.timeScale;
+
+        while(time < landLerpTime)
+        {
+            time += Time.deltaTime;
+            Time.timeScale = Mathf.Lerp(initialTimeScale, targetTimeScale, time / slowMoTime);
+            yield return null;
+        }
+
+        Time.timeScale = targetTimeScale;
+    }
 }
