3ffed1e240645d978c14a537e283d29e0224b850
Jacob Green <56412581+jacobgreendev@users.noreply.github.com>
2021-08-07 03:02:16 +0100

    LootLocker leaderboard intergration initial commit

--- WispRemasteredUnity/Assets/Scripts/ScoreManager.cs
+++ WispRemasteredUnity/Assets/Scripts/ScoreManager.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using LootLocker.Requests;
 
 public class ScoreManager : MonoBehaviour
 {
@@ -23,6 +24,24 @@ public class ScoreManager : MonoBehaviour
     void Start()
     {
         score = 0;
+        LootLockerSDKManager.StartSession("TestDevice", (response) =>
+        {
+            Debug.Log("Started session success");
+        });
+
+        LootLockerSDKManager.SetPlayerName("TestPlayer", (response) =>
+        {
+            Debug.Log("Player name set successfully");
+        });
+
+        LootLockerSDKManager.GetScoreList(322, 1, (response) =>
+        {
+            if (response.success && response.items.Length > 0)
+            {
+                LootLockerLeaderboardMember topScore = response.items[0];
+                Debug.Log($"Current record held by {topScore.player.name} is {topScore.score}");
+            }
+        });
     }
 
     private void OnDisable()
@@ -45,10 +64,16 @@ public class ScoreManager : MonoBehaviour
     {
         PlayerSaveManager.LoadSaveFile();
         var saveData = LocalSaveData.Instance;
-        if(score > saveData.hiScore)
+        if (score > saveData.hiScore)
         {
             saveData.hiScore = score;
         }
+
+        LootLockerSDKManager.SubmitScore("TestPlayer", score, 322, (response) =>
+        {
+            Debug.Log("Submitted score success");
+        });
+
         PlayerSaveManager.SaveFile();
     }
 
