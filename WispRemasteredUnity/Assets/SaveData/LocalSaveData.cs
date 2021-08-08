f9da9e1b1649b3c0cfef287acd167cab53773b95
Jacob Green <56412581+jacobgreendev@users.noreply.github.com>
2021-08-08 04:12:12 +0100

    Added version tolerant serialization attributes to LocalSaveData so that old save data can still be deserialized

--- WispRemasteredUnity/Assets/SaveData/LocalSaveData.cs
+++ WispRemasteredUnity/Assets/SaveData/LocalSaveData.cs
@@ -6,11 +6,14 @@ public class LocalSaveData
 {
     public static LocalSaveData Instance;
 
+    [OptionalField(VersionAdded = 2)]
     public int endlessHiScore = 0;
+
+    [OptionalField(VersionAdded = 2)] 
     public Dictionary<int, Dictionary<int, int>> levelScores = new Dictionary<int, Dictionary<int, int>>();
 
     [OnDeserialized]
-    void OnDeserialized()
+    void OnDeserialized(StreamingContext context)
     {
         if(levelScores == null)
         {
